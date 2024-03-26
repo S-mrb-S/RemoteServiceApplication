4229c79c4a265abf3688fecbe1cf76b513523b06
MehraB832 <mehrabshafae@gmail.com>
2024-03-26 15:33:02 +0330

    removed .

--- app/src/main/java/sp/remoteapplication/MainActivity.kt
+++ app/src/main/java/sp/remoteapplication/MainActivity.kt
@@ -29,12 +29,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import sp.remoteapplication.model.RemoteWindowsType
 import sp.remoteapplication.ui.theme.RemoteApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
-    private var packageManager: PackageManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,10 +54,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
-        packageManager = getPackageManager()
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        RemoteWindowsType(windowManager)
     }
 }
 
@@ -89,62 +83,16 @@ fun ActionButton(text: String) {
     Button(
         onClick = {
             try {
-//                val packageName = "com.cisco.anyconnect.vpn.android.avf"
-//                val isInstalled = isAppInstalled(context, packageName)
-//                if (isInstalled) {
-//                    // برنامه وجود دارد
-//                    Toast.makeText(context, "yes", Toast.LENGTH_SHORT).show()
-//
-//                    val intent = context.packageManager.getLaunchIntentForPackage(packageName)
-//                    if (intent != null) {
-//                        // We found the activity now start the activity
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-//                        context.startActivity(intent)
-//                    } else {
-//                        Toast.makeText(context, "noop 2", Toast.LENGTH_SHORT).show()
-//                    }
-//                } else {
-//                    // برنامه وجود ندارد
-//                    Toast.makeText(context, "noop", Toast.LENGTH_SHORT).show()
-//                }
-
-//                Toast.makeText(context, "access!", Toast.LENGTH_SHORT).show()
-//                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-//                context.startActivity(intent)
-
-//                Toast.makeText(context, "Service started!", Toast.LENGTH_SHORT).show()
-//                val serviceIntent = Intent(context, RemoteAccessibilityService::class.java)
-//                context.startService(serviceIntent)
-
-//                val serviceIntent = Intent(context, MyService::class.java)
-//                context.startService(serviceIntent)
-
-                val intent =
-                    context.packageManager.getLaunchIntentForPackage("com.cisco.anyconnect.vpn.android.avf")
-                intent?.let {
-                    it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    context.startActivity(it)
+                val packageName = "com.cisco.anyconnect.vpn.android.avf"
+                val isInstalled = isAppInstalled(context, packageName)
+                if (isInstalled) {
+                    // برنامه وجود دارد
+                    Toast.makeText(context, "yes", Toast.LENGTH_SHORT).show()
+                    
+                } else {
+                    // برنامه وجود ندارد
+                    Toast.makeText(context, "noop", Toast.LENGTH_SHORT).show()
                 }
-
-                val params = WindowManager.LayoutParams(
-                    WindowManager.LayoutParams.MATCH_PARENT,
-                    WindowManager.LayoutParams.MATCH_PARENT,
-                    WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG,
-                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or
-                            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
-                            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
-                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-                    PixelFormat.TRANSLUCENT
-                )
-
-                val view = View(context)
-                view.setBackgroundColor(Color.TRANSPARENT)
-
-                RemoteWindowsType.getPackageManager().addView(view, params)
-
             } catch (e: Exception) {
                 Toast.makeText(context, "Ex", Toast.LENGTH_SHORT).show()
                 Log.e("Exepction: ", e.toString())
